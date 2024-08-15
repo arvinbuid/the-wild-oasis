661586aa85550182679c21b1d16afeae214d2cff
Arvin Buid <arvinbuid01@gmail.com>
2024-08-15 21:43:58 +0800

    Created a reusable Row component and used it inside App component file. Added default props for reusable Button component.

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,43 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input/";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-  background-color: orangered;
   padding: 20px;
 `;
 
 function App() {
   return (
     <>
-      <GlobalStyles />
-      <StyledApp>
-        <Heading as='h1'>The Wild Oasis</Heading>
-        <Heading as='h2'>Check in and out</Heading>
-        <Button onClick={() => alert("Check in")}>Check In</Button>
-        <Button onClick={() => alert("Check out")}>Check Out</Button>
+      <Row>
+        <GlobalStyles />
+        <StyledApp>
+          <Row type='horizontal'>
+            <Heading as='h1'>The Wild Oasis</Heading>
 
-        <Heading as='h3'>Form</Heading>
-        <Input type='number' placeholder='Number of guests' />
-        <Input type='number' placeholder='Number of guests' />
-      </StyledApp>
+            <div>
+              <Heading as='h2'>Check in and out</Heading>
+              <Button onClick={() => alert("Check in")}>Check In</Button>
+              <Button variation='secondary' size='medium' onClick={() => alert("Check out")}>
+                Check Out
+              </Button>
+            </div>
+          </Row>
+
+          <Row>
+            <Heading as='h3'>Form</Heading>
+
+            <div>
+              <Input type='number' placeholder='Number of guests' />
+              <Input type='number' placeholder='Number of guests' />
+            </div>
+          </Row>
+        </StyledApp>
+      </Row>
     </>
   );
 }
 
+// defaultProps
 export default App;
