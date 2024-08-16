3d03bc8e501e90c67101687a4578640e0a1ca9f8
Arvin Buid <arvinbuid01@gmail.com>
2024-08-16 18:49:19 +0800

    Added supabase credentials.

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import {createClient} from "@supabase/supabase-js";
+const supabaseUrl = "https://dzvzphczalsmiclfphyv.supabase.co";
+// eslint-disable-next-line no-undef
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
