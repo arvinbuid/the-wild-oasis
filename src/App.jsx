17c279e17e2bbce874acd7635a4a118313e2b27a
Arvin Buid <arvinbuid01@gmail.com>
2024-08-16 22:11:04 +0800

    Configured react query devtools inside the App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import {Fragment} from "react";
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 
 import GlobalStyles from "./styles/GlobalStyles";
 import Dashboard from "./pages/Dashboard";
@@ -24,6 +24,7 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
