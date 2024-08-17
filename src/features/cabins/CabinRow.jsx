9480b74629ad05e581a560f3eebd0e78d64d1886
Arvin Buid <arvinbuid01@gmail.com>
2024-08-17 21:00:40 +0800

    Define and configure Toaster and its props inside App component. Used toaster success and error when deleting a cabin & define Container styled component to style the Outlet component max-width and spacing.

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -2,6 +2,7 @@ import {useMutation, useQueryClient} from "@tanstack/react-query";
 import {formatCurrency} from "../../utils/helpers";
 import {deleteCabin} from "../../services/apiCabins";
 import styled from "styled-components";
+import toast from "react-hot-toast";
 
 const TableRow = styled.div`
   display: grid;
@@ -50,13 +51,13 @@ function CabinRow({cabin}) {
   const {isLoading: isDeleting, mutate} = useMutation({
     mutationFn: deleteCabin,
     onSuccess: () => {
-      alert("Cabin successfully deleted.");
+      toast.success("Cabin successfully deleted.");
 
       queryClient.invalidateQueries({
         queryKey: ["cabins"],
       });
     },
-    onError: (err) => alert(err.message),
+    onError: (err) => toast.error(err.message),
   });
 
   return (
