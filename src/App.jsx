b99597ff6641313043c910b84ccdeab3e971fecc
Arvin Buid <arvinbuid01@gmail.com>
2024-08-17 21:45:56 +0800

    Add a button and a state to conditionally display CreateCabinForm component inside Cabins component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import {StyleSheetManager} from "styled-components";
 
 import GlobalStyles from "./styles/GlobalStyles";
 import Dashboard from "./pages/Dashboard";
@@ -24,47 +25,49 @@ const queryClient = new QueryClient({
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
-      <GlobalStyles />
-      <BrowserRouter>
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route index element={<Navigate replace to='dashboard' />} />
-            <Route path='dashboard' element={<Dashboard />} />
-            <Route path='bookings' element={<Bookings />} />
-            <Route path='cabins' element={<Cabins />} />
-            <Route path='users' element={<Users />} />
-            <Route path='settings' element={<Settings />} />
-            <Route path='account' element={<Account />} />
-          </Route>
+    <StyleSheetManager shouldForwardProp={(prop) => prop !== "variation"}>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <GlobalStyles />
+        <BrowserRouter>
+          <Routes>
+            <Route element={<AppLayout />}>
+              <Route index element={<Navigate replace to='dashboard' />} />
+              <Route path='dashboard' element={<Dashboard />} />
+              <Route path='bookings' element={<Bookings />} />
+              <Route path='cabins' element={<Cabins />} />
+              <Route path='users' element={<Users />} />
+              <Route path='settings' element={<Settings />} />
+              <Route path='account' element={<Account />} />
+            </Route>
 
-          <Route path='login' element={<Login />} />
-          <Route path='*' element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
+            <Route path='login' element={<Login />} />
+            <Route path='*' element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
 
-      <Toaster
-        position='top-center'
-        gutter={12}
-        containerStyle={{margin: "8px"}}
-        toastOptions={{
-          success: {
-            duration: 3000,
-          },
-          error: {
-            duration: 5000,
-          },
-          style: {
-            fontSize: "16px",
-            maxWidth: "500px",
-            padding: "16px 24px",
-            backgroundColor: "var(--color-grey-0)",
-            color: "var(--color-grey-700",
-          },
-        }}
-      />
-    </QueryClientProvider>
+        <Toaster
+          position='top-center'
+          gutter={12}
+          containerStyle={{margin: "8px"}}
+          toastOptions={{
+            success: {
+              duration: 3000,
+            },
+            error: {
+              duration: 5000,
+            },
+            style: {
+              fontSize: "16px",
+              maxWidth: "500px",
+              padding: "16px 24px",
+              backgroundColor: "var(--color-grey-0)",
+              color: "var(--color-grey-700",
+            },
+          }}
+        />
+      </QueryClientProvider>
+    </StyleSheetManager>
   );
 }
 
