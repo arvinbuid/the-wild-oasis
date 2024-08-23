e097edbfb86f08d521d8ea1096f0bd8bea7e988b
Arvin Buid <arvinbuid01@gmail.com>
2024-08-23 21:11:05 +0800

    Created Checkin Component, useCheckin custom hook, and add a checkin route inside App Component to implement checkin a booking of a customer.

--- src/features/check-in-out/CheckinBooking.jsx
+++ src/features/check-in-out/CheckinBooking.jsx
@@ -1,3 +1,6 @@
+import {useEffect, useState} from "react";
+import {useCheckin} from "./useCheckin";
+import {useMoveBack} from "../../hooks/useMoveBack";
 import styled from "styled-components";
 import BookingDataBox from "../../features/bookings/BookingDataBox";
 
@@ -7,7 +10,9 @@ import ButtonGroup from "../../ui/ButtonGroup";
 import Button from "../../ui/Button";
 import ButtonText from "../../ui/ButtonText";
 
-import { useMoveBack } from "../../hooks/useMoveBack";
+import useBooking from "../bookings/useBooking";
+import Spinner from "../../ui/Spinner";
+import Checkbox from "../../ui/Checkbox";
 
 const Box = styled.div`
   /* Box */
@@ -18,33 +23,61 @@ const Box = styled.div`
 `;
 
 function CheckinBooking() {
+  const [confirmPaid, setConfirmPaid] = useState(false);
+  // const [addBreakfast, setAddBreakfast] = useState(false);
+  const {booking, isLoading} = useBooking();
+  const {checkin, isCheckingIn} = useCheckin();
   const moveBack = useMoveBack();
 
-  const booking = {};
+  useEffect(() => setConfirmPaid(booking?.isPaid ?? false), [booking]);
 
-  const {
-    id: bookingId,
-    guests,
-    totalPrice,
-    numGuests,
-    hasBreakfast,
-    numNights,
-  } = booking;
+  if (isLoading) return <Spinner />;
 
-  function handleCheckin() {}
+  const {id: bookingId, guests, totalPrice, numGuests, hasBreakfast, numNights, isPaid} = booking;
+
+  function handleCheckin() {
+    if (!confirmPaid) return;
+    checkin(bookingId);
+  }
 
   return (
     <>
-      <Row type="horizontal">
-        <Heading as="h1">Check in booking #{bookingId}</Heading>
+      <Row type='horizontal'>
+        <Heading as='h1'>Check in booking #{bookingId}</Heading>
         <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
       </Row>
 
       <BookingDataBox booking={booking} />
 
+      {/* <Box>
+        <Checkbox
+          checked={addBreakfast}
+          onChange={() => {
+            setAddBreakfast((add) => !add);
+            setConfirmPaid(false);
+          }}
+          id='breakfast'
+        >
+          Want to add breakfast for X price?
+        </Checkbox>
+      </Box> */}
+
+      <Box>
+        <Checkbox
+          checked={confirmPaid}
+          onChange={() => setConfirmPaid((confirm) => !confirm)}
+          id='confirmPaid'
+          disabled={isPaid}
+        >
+          I confirm that {guests.fullName} has paid the total amount
+        </Checkbox>
+      </Box>
+
       <ButtonGroup>
-        <Button onClick={handleCheckin}>Check in booking #{bookingId}</Button>
-        <Button variation="secondary" onClick={moveBack}>
+        <Button onClick={handleCheckin} disabled={!confirmPaid || isCheckingIn}>
+          Check in booking #{bookingId}
+        </Button>
+        <Button variation='secondary' onClick={moveBack} disabled={isCheckingIn}>
           Back
         </Button>
       </ButtonGroup>
--- src/ui/Checkbox.jsx
+++ src/ui/Checkbox.jsx
@@ -25,16 +25,10 @@ const StyledCheckbox = styled.div`
   }
 `;
 
-function Checkbox({ checked, onChange, disabled = false, id, children }) {
+function Checkbox({checked, onChange, disabled = false, id, children}) {
   return (
     <StyledCheckbox>
-      <input
-        type="checkbox"
-        id={id}
-        checked={checked}
-        onChange={onChange}
-        disabled={disabled}
-      />
+      <input type='checkbox' id={id} checked={checked} onChange={onChange} disabled={disabled} />
       <label htmlFor={!disabled ? id : ""}>{children}</label>
     </StyledCheckbox>
   );
