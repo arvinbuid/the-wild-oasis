90e9078a8ebf7116884177db1b350e3a7418779d
Arvin Buid <arvinbuid01@gmail.com>
2024-08-25 13:54:32 +0800

    Created updateCurrentUser function inside apiAuth.js file, created useUpdateUser custom hook to update the user full name, password and avatar.

--- src/features/authentication/UpdateUserDataForm.jsx
+++ src/features/authentication/UpdateUserDataForm.jsx
@@ -1,54 +1,67 @@
-import { useState } from "react";
-
+import {useState} from "react";
+import {useUser} from "./useUser";
 import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
-
-import { useUser } from "./useUser";
+import useUpdateUser from "./useUpdateUser";
 
 function UpdateUserDataForm() {
-  // We don't need the loading state, and can immediately use the user data, because we know that it has already been loaded at this point
+  // Loading state is not need, and can immediately use the user data, because the data has already been loaded at this point
   const {
     user: {
       email,
-      user_metadata: { fullName: currentFullName },
+      user_metadata: {fullName: currentFullName},
     },
   } = useUser();
 
   const [fullName, setFullName] = useState(currentFullName);
   const [avatar, setAvatar] = useState(null);
+  const {updateUser, isUpdatingUser} = useUpdateUser();
 
   function handleSubmit(e) {
     e.preventDefault();
+    if (!fullName) return;
+
+    updateUser(
+      {fullName, avatar},
+      {
+        onSuccess: () => {
+          setAvatar(null);
+          e.target.reset();
+        },
+      }
+    );
+  }
+
+  function handleClick() {
+    setFullName(currentFullName);
+    setAvatar("");
   }
 
   return (
     <Form onSubmit={handleSubmit}>
-      <FormRow label="Email address">
+      <FormRow label='Email address'>
         <Input value={email} disabled />
       </FormRow>
-      <FormRow label="Full name">
+      <FormRow label='Full name'>
         <Input
-          type="text"
+          type='text'
           value={fullName}
           onChange={(e) => setFullName(e.target.value)}
-          id="fullName"
+          id='fullName'
+          disabled={isUpdatingUser}
         />
       </FormRow>
-      <FormRow label="Avatar image">
-        <FileInput
-          id="avatar"
-          accept="image/*"
-          onChange={(e) => setAvatar(e.target.files[0])}
-        />
+      <FormRow label='Avatar image'>
+        <FileInput id='avatar' accept='image/*' onChange={(e) => setAvatar(e.target.files[0])} />
       </FormRow>
       <FormRow>
-        <Button type="reset" variation="secondary">
+        <Button type='reset' variation='secondary' disabled={isUpdatingUser} onClick={handleClick}>
           Cancel
         </Button>
-        <Button>Update account</Button>
+        <Button disabled={isUpdatingUser}>Update account</Button>
       </FormRow>
     </Form>
   );
