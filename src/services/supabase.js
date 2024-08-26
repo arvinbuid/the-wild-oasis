c98a5ac196af9e22dcc813300f7d7556abca8bae
Arvin Buid <arvinbuid01@gmail.com>
2024-08-27 00:28:07 +0800

    Minor changes for Vercel deploy.

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,8 @@
 import {createClient} from "@supabase/supabase-js";
 export const supabaseUrl = "https://dzvzphczalsmiclfphyv.supabase.co";
 // eslint-disable-next-line no-undef
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6dnpwaGN6YWxzbWljbGZwaHl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM3MzkyMTksImV4cCI6MjAzOTMxNTIxOX0.SgAmxtbjfmR66ctT6mjX4vBsMowuwh0s6Q3CXWN-b1o";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
